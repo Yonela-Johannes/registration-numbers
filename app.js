c4d6209a23ff1f1db4a323247f65527f4c3891f7
Yonela-Johannes <johannesyonela@gmail.com>
2022-05-18 03:26:00 +0200

    switching dom and factory functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // getting function
 import RegistrationNumbers from './main.js'
-const registrationNumbers = RegistrationNumbers()
+const regNumbers = RegistrationNumbers()
 // getting and init document object elements
 const towns = document.querySelector('select')
 const townName = document.querySelector('.display-name')
@@ -10,78 +10,80 @@ const errorHandlerWrapper = document.querySelector('.error-handling-wrapper')
 const regInput = document.querySelector('.reg-input')
 const addNumber = document.querySelector('.add-number')
 const resetRegNumbers = document.querySelector('.reset-numbers')
-// end of selecting dom elements
-// getting localstorage and setting if empty
-let numbers = JSON.parse(localStorage.getItem('regNumbers')) ? JSON.parse(localStorage.getItem('regNumbers')) : localStorage.setItem("regNumbers", JSON.stringify([]))
-let oldRegNum = numbers ? numbers : []
+
+JSON.parse(localStorage.getItem('regNumbers')) == null && localStorage.setItem("regNumbers", JSON.stringify([]))
+
+let numbers = JSON.parse(localStorage.getItem('regNumbers'))
+
 townName.innerHTML = "No Town Selected"
-// printing local storage data to document
-registrationNumbers.storedRegis(filteredNumberPlate, oldRegNum)
+// loop add, and store 
+const storedRegis = (numberPlate, array) => {
+    numberPlate.innerHTML = ""
+    array.forEach(element =>
+        numberPlate.innerHTML += `
+                <div class='list'>${element}</div>
+            `
+    )
+}
+
+const resetErrorHandlers = (errorHandler, errorHandlerWrapper) => {
+    // Time out for poppup messages
+    setTimeout(() => {
+        // clear handler from dom
+        errorHandler.classList.remove('fail'),
+            errorHandler.classList.remove('success'),
+            errorHandlerWrapper.classList.add("hide")
+    }, 3000)
+}
+
 // init app
 const addRegNo = (e) => {
     // cancel event
-    e.preventDefault()
-    // reseting dropdown
+    // e.preventDefault()
     towns.selectedIndex = 0
     townName.innerHTML = "No Town Selected"
-    // init data
     let number = regInput.value
-    // passing input
-    registrationNumbers.setRegNumber(number)
-    // validate if registration format is correct
-    const regPlateInputFormat = registrationNumbers.validateRegistrationNr(number)
-    // if registatrion is a correct Western Cape string
-    const checkRegistration = registrationNumbers.checkReg(registrationNumbers.regisAbreviations())
-    // checking registration number using REGEX and JS methods
+    regNumbers.setNumber(number)
+    JSON.parse(localStorage.getItem("regNumbers")),
 
-    regPlateInputFormat & typeof number == 'string' & checkRegistration ? (
-        // Checking if number already exists
-        !registrationNumbers.checkIfRegExist(oldRegNum, number) ? (
-            // setting entered number
-            registrationNumbers.setRegNumber(number),
-            // start of success error handlers
-            errorHandlerWrapper.classList.remove("hide"),
-            errorHandler.classList.add('success'),
-            errorHandler.innerHTML = 'Number added successfully',
-            // end of dom succes handlers
-            // storing data to local storage
-            oldRegNum = JSON.parse(localStorage.getItem('regNumbers')),
-            oldRegNum.push(registrationNumbers.getRegNumber()),
-            localStorage.setItem("regNumbers", JSON.stringify(oldRegNum)),
-            registrationNumbers.storedRegis(filteredNumberPlate, oldRegNum),
-            regInput.value = ''
-        ) :
-            // start of error dom handlers
-            (
+
+        !regNumbers.checkRegExist() && (
+            regNumbers.getRegNumber() && regNumbers.validNo() && regNumbers.checkReg() && (
+                regNumbers.setNumber(number),
                 errorHandlerWrapper.classList.remove("hide"),
-                errorHandler.classList.add('fail'),
-                errorHandler.innerHTML = 'Number exist in storage',
+                errorHandler.classList.add('success'),
+                errorHandler.innerHTML = 'Number added successfully',
+                regNumbers.setRegNumbers(numbers),
+                console.log(regNumbers.getNumbers()),
+                localStorage.setItem("regNumbers", JSON.stringify(numbers)),
+                storedRegis(filteredNumberPlate, numbers),
                 regInput.value = ''
-            )
-        // end of success dom handlers
-    ) :
-        // Checking if input is emty and returning an error
-        number == "" ? (
-            // start of error dom handlers
-            // toggling node elements
-            errorHandlerWrapper.classList.remove("hide"),
-            errorHandler.classList.add('fail'),
-            errorHandler.innerHTML = 'Enter registration please',
-            registrationNumbers.storedRegis(filteredNumberPlate, oldRegNum)
-        ) :
-            // Checking if number is entered in the correct format
-            !regPlateInputFormat ? (
-                // start of error dom handlers
-                // toggling node elements
-                errorHandlerWrapper.classList.remove("hide"),
-                errorHandler.classList.add('fail'),
-                errorHandler.innerHTML = 'Enter correct registration, e.g: ca 123-456 or CT 123')
-                : !number.includes(registrationNumbers.regisAbreviations()) && (errorHandlerWrapper.classList.remove("hide"),
-                    errorHandler.classList.add('fail'),
-                    errorHandler.innerHTML = 'Enter correct town registration format, e.g: ca 123-456 or CF 123')
-    // end of error dom handlers
-    // Time out for poppup messages
-    registrationNumbers.resetErrorHandlers(errorHandler, errorHandlerWrapper)
+            ))
+
+    regNumbers.checkRegExist() && (
+        errorHandlerWrapper.classList.remove("hide"),
+        errorHandler.classList.add('fail'),
+        errorHandler.innerHTML = 'Number exist in storage',
+        regInput.value = ''
+    )
+
+
+    regNumbers.getRegNumber() == "" && (
+        towns.selectedIndex = 0,
+        errorHandlerWrapper.classList.remove("hide"),
+        errorHandler.classList.add('fail'),
+        errorHandler.innerHTML = 'Enter registration please')
+
+    !regNumbers.validNo() && (
+        errorHandlerWrapper.classList.remove("hide"),
+        errorHandler.classList.add('fail'),
+        errorHandler.innerHTML = 'Enter correct registration, e.g: ca 123-456 or CT 123')
+
+    !!regNumbers.checkPrefixAndAffix() && (errorHandlerWrapper.classList.remove("hide"),
+        errorHandler.classList.add('fail'),
+        errorHandler.innerHTML = 'Enter correct registration number format, e.g: ca 123-456 or CF 123')
+    resetErrorHandlers(errorHandler, errorHandlerWrapper)
+    filteredNumberPlate.innerHTML = ""
 }
 // listening for a click even on the dom
 addNumber.addEventListener('click', addRegNo)
@@ -92,25 +94,27 @@ const selectTown = () => {
     const displayName = towns.options[towns.selectedIndex].textContent
     townName.innerHTML = displayName
     // filter towns
-    registrationNumbers.setByTown(town, oldRegNum)
-    registrationNumbers.getTownReg().length == 0 ? filteredNumberPlate.innerHTML = `There is no registration numbers for ${displayName}` :
-        registrationNumbers.storedRegis(filteredNumberPlate, registrationNumbers.getTownReg())
+    regNumbers.setByTown(town, numbers)
+    regNumbers.getTownReg().length == 0 ? filteredNumberPlate.innerHTML = `There is no registration numbers for ${displayName}` :
+        storedRegis(filteredNumberPlate, regNumbers.getTownReg())
     // set time out for error handlers
-    registrationNumbers.getTownReg().length == 0 &&
+    regNumbers.getTownReg().length == 0 &&
         setTimeout(() => {
+            towns.selectedIndex = 0
+            townName.innerHTML = "not town selected"
             filteredNumberPlate.innerHTML = ""
-        }, 3700)
+        }, 3500)
 }
 // listening for a change event on the dom
 towns.addEventListener('change', selectTown)
 // listening for a change event on the dom
 
 const clearRegNumbers = () => {
-    !!oldRegNum && (errorHandlerWrapper.classList.remove("hide"),
+    !!numbers && (errorHandlerWrapper.classList.remove("hide"),
         errorHandler.classList.add('fail'),
         errorHandler.innerHTML = 'Registration numbers are already cleared')
     localStorage.clear('regNumbers')
-    registrationNumbers.resetErrorHandlers(errorHandler, errorHandlerWrapper)
+    resetErrorHandlers(errorHandler, errorHandlerWrapper)
     location.reload()
 }
 
--- main.js
+++ main.js
@@ -1,43 +1,35 @@
 const RegistrationNumbers = () => {
     let number = ''
     let selectedTownReg = []
-
-    // Western province registration abbreviations
+    let newList = []
+    let existNo = false
+    const regex = /[a-zA-Z]{1,2}\s[0-9]{2,5}(\s|\-)?([0-9]{2,3})?[a-zA-Z]?/
     const regisAbreviations = () => {
         const indexes = ['ca', 'caa', 'wp', 'cy', 'cf', 'cn', 'cl', 'ck', 'cw', 'ct', 'caw', 'cf', 'cj']
         return indexes.map(el => el.toUpperCase())
     }
-    // loop add, and store 
-    const storedRegis = (numberPlate, array) => {
-        numberPlate.innerHTML = ""
-        array.forEach(element =>
-            numberPlate.innerHTML += `
-                <div class='list'>${element}</div>
-            `
-        )
+    const setNumber = (reg) => {
+        number = reg.toUpperCase().trim()
     }
-    // Set/Get/Validate number
-    const setRegNumber = regNo => number = regNo.toUpperCase().trim()
+
     const getRegNumber = () => number
-    // Test Input string using Regex
-    const validateRegistrationNr = number => {
-        const regex = /[a-zA-Z]{1,2}\s[0-9]{2,5}(\s|\-)?([0-9]{2,3})?[a-zA-Z]?/
-        return regex.test(number)
-    }
-    // Check if new number exist of not
-    const checkIfRegExist = (list, number) => {
-        let result = list.find(regno => regno == number.toUpperCase()) ? true : false
-        return result
-    }
-    // check if input starts with correct letters
-    const checkReg = array => {
+
+    const validNo = () => regex.test(getRegNumber())
+
+    const setRegNumbers = (list) => list.find(regNo => regNo == getRegNumber()) ? (newList = list, existNo = true) : (newList = list.push(getRegNumber()), existNo = false)
+    const getNumbers = () => newList
+    const checkRegExist = () => existNo
+
+    const checkPrefixAndAffix = () => regisAbreviations().includes(getRegNumber())
+
+    const checkReg = () => {
         let introArray = false
-        for (let i = 0; i < array.length; i++) {
-            if (getRegNumber().startsWith(array[i])) introArray = true
+        for (let i = 0; i < regisAbreviations().length; i++) {
+            if (getRegNumber().startsWith(regisAbreviations()[i])) introArray = true
         }
         return introArray
     }
-    // Get registration by town names
+
     const setTownReg = (string, array) => {
         selectedTownReg = array.filter(arr => arr.startsWith(string))
     }
@@ -48,27 +40,19 @@ const RegistrationNumbers = () => {
 
     const getTownReg = () => selectedTownReg
 
-    const resetErrorHandlers = (errorHandler,errorHandlerWrapper) => {
-        // Time out for poppup messages
-        setTimeout(() => {
-            // clear handler from dom
-            errorHandler.classList.remove('fail'),
-                errorHandler.classList.remove('success'),
-                errorHandlerWrapper.classList.add("hide")
-        }, 3000)
-    }
     return {
+        setNumber,
         regisAbreviations,
-        setRegNumber,
         getRegNumber,
+        validNo,
+        setRegNumbers,
+        getNumbers,
+        checkPrefixAndAffix,
         checkReg,
-        checkIfRegExist,
-        storedRegis,
+        checkRegExist,
         setTownReg,
         getTownReg,
-        validateRegistrationNr,
         setByTown,
-        resetErrorHandlers
     }
 }
 
